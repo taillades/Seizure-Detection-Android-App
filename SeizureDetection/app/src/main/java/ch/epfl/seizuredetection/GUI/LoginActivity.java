2efc2a3415afa03e7d27039e47a5c5c5efda3dc8
maroacc <45521548+maroacc@users.noreply.github.com>
2020-12-11 15:51:33 +0100

    Login done. Has to retrieve the user from the realtime database yet tho

--- SeizureDetection/app/src/main/java/ch/epfl/seizuredetection/GUI/LoginActivity.java
+++ SeizureDetection/app/src/main/java/ch/epfl/seizuredetection/GUI/LoginActivity.java
@@ -3,7 +3,6 @@ package ch.epfl.seizuredetection.GUI;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.SpannableString;
@@ -11,17 +10,26 @@ import android.text.Spanned;
 import android.text.TextPaint;
 import android.text.method.LinkMovementMethod;
 import android.text.style.ClickableSpan;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import ch.epfl.seizuredetection.DAO.ProfileDAO;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import ch.epfl.seizuredetection.R;
 
 public class LoginActivity extends AppCompatActivity {
-    EditText LoginEmail;
-    EditText LoginPwd;
+    private FirebaseAuth mAuth; // Auth database instance
+    private final String TAG = this.getClass().getSimpleName();
+    EditText editTextEmail;
+    EditText editTextPassword;
     Button LoginButton;
     String ID;
     TextView SignUp;
@@ -31,8 +39,9 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        LoginEmail = findViewById(R.id.LoginEmail);
-        LoginPwd = findViewById(R.id.LoginPwd);
+        mAuth = FirebaseAuth.getInstance(); // Auth database instance
+        editTextEmail = findViewById(R.id.LoginEmail);
+        editTextPassword = findViewById(R.id.LoginPwd);
         LoginButton = findViewById(R.id.LoginButton);
         SignUp = findViewById(R.id.SignUp);
 
@@ -62,12 +71,32 @@ public class LoginActivity extends AppCompatActivity {
         LoginButton.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
-                String email = LoginEmail.getText().toString();
-                String pwd = LoginPwd.getText().toString();
+                String email = editTextEmail.getText().toString();
+                String pwd = editTextPassword.getText().toString();
+                login(email, pwd);
 
-                ID = ProfileDAO.login(email,pwd);
             }
         });
      }
 
+    private void login(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(LoginActivity.this, "Authentication success.", Toast.LENGTH_SHORT).show();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            editTextPassword.setError("Wrong email or password");
+                            editTextPassword.requestFocus();
+                            Toast.makeText(LoginActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
 }
